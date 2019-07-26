5d866c25aa6206aaef00de63043623a0ae2dbec8
Erickson Reyes <ercbluemonday@yahoo.com>
2019-07-26 17:41:57 +0800

    Make code base 100% test covered

--- spec/EricksonReyes/DomainDrivenDesign/Common/Factory/PersonNameFactorySpec.php
+++ spec/EricksonReyes/DomainDrivenDesign/Common/Factory/PersonNameFactorySpec.php
@@ -2,6 +2,8 @@
 
 namespace spec\EricksonReyes\DomainDrivenDesign\Common\Factory;
 
+use EricksonReyes\DomainDrivenDesign\Common\Factory\Exception\MissingFirstNameException;
+use EricksonReyes\DomainDrivenDesign\Common\Factory\Exception\MissingLastNameException;
 use EricksonReyes\DomainDrivenDesign\Common\Factory\PersonNameFactory;
 use EricksonReyes\DomainDrivenDesign\Common\ValueObject\PersonName;
 use PhpSpec\ObjectBehavior;
@@ -19,11 +21,35 @@ class PersonNameFactorySpec extends ObjectBehavior
     public function it_creates_person_names()
     {
         $this::create(
-            $expectedLastName = $this->seeder->lastName,
-            $expectedFirstName = $this->seeder->firstName,
-            $expectedMiddleName = $this->seeder->lastName,
-            $expectedPostNominal = $this->seeder->randomLetter,
-            $expectedHonorific = $this->seeder->title
+            $this->seeder->lastName,
+            $this->seeder->firstName,
+            $this->seeder->lastName,
+            $this->seeder->randomLetter,
+            $this->seeder->title
         )->shouldHaveType(PersonName::class);
+    }
+
+    public function it_requires_a_last_name()
+    {
+        $this->shouldThrow(MissingLastNameException::class)
+            ->during(
+                'create',
+                [
+                    str_repeat(' ', random_int(0, 10)),
+                    $this->seeder->firstName
+                ]
+            );
+    }
+
+    public function it_requires_a_first_name()
+    {
+        $this->shouldThrow(MissingFirstNameException::class)
+            ->during(
+                'create',
+                [
+                    $this->seeder->lastName,
+                    str_repeat(' ', random_int(0, 10))
+                ]
+            );
     }
 }
